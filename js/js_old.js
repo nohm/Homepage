9b7fcf9d807345c4f7e83ae8750b12a1fa01b905
Steven Koolhaas <snack93@lavabit.com>
2013-02-27 22:59:27 +0100

    Fonts.

--- js/js_old.js
+++ js/js_old.js
@@ -17,63 +17,75 @@ addEvent(window, 'load', showAvatar); 	// set the avatar
 addEvent(window, 'load', updateWeather);// get the weather
 
 // WEATHER //
-var loc = 'wilsum';
-var url = 'http://api.wunderground.com/api/bf6fcb121000e936/geolookup/conditions/q/' + encodeURIComponent(loc) + '.json'; 
-function updateWeather() {  
+function updateWeather() {
+    var loc = 'wilsum';
+    //var u = 'c';
+    //var query = "SELECT * FROM weather.forecast WHERE woeid IN (SELECT woeid FROM geo.places WHERE text='" + loc + "') AND u='" + u + "'";
+    //var url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + '&format=json';
+    var url = 'http://api.wunderground.com/api/bf6fcb121000e936/geolookup/conditions/q/' + encodeURIComponent(loc) + '.json';
+    
     $.ajax({
-        dataType: 'jsonp',
+        dataType: 'jsonp', // this automatically disables cache too
         url: url,
         timeout: 5000,
         success: function(data) {
         	 var location = data['location']['city'];
         	 var type = data['current_observation']['weather'];
-        	 if (type.length > 15) type = type.substring(0, 12) + '...' // Cut off ridiculous names
         	 var temp_c = data['current_observation']['temp_c'];
         	 document.getElementById('weather').innerHTML = location + '<br />' + type + '<br />' + temp_c + '&deg;C';
+        	//var info = data.query.results.channel.item.condition;
+            //document.getElementById('weather').innerHTML = info.text + " " + info.temp + '&deg;' + (u.toUpperCase());
         },
     	error: function() {
-             document.getElementById('weather').innerHTML = '';
+            document.getElementById('weather').innerHTML = "";
         }
     });
-    t = setTimeout('updateWeather()',900000); // Update after 15m
+    t = setTimeout('updateWeather()',900000);
 }
 
 // CLOCK/DATE //
+// Needed for full date // var monthNames=[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+// Needed for full date // var dayNames=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-	if (h < 10) h = '0'+h;
-	if (m < 10) m = '0'+m;
-	if (s < 10) s = '0'+s;
-	document.getElementById('date').innerHTML = h + ':' + m + ':' + s + '<br />' + today.getDate() + '/' + (today.getMonth()+1) + '/' + today.getFullYear();
+    /* AM/PM add 'a+' after 's+' to use
+    var a = "am";
+    if (h > 12) h = h-12; a="pm";*/
+	if (h < 10) h = "0"+h;
+	if (m < 10) m = "0"+m;
+	if (s < 10) s = "0"+s;
+	// Full date
+    //document.getElementById('date').innerHTML = h+":"+m+":"+s+"<br />"+dayNames[today.getDay()]+"<br />"+today.getDate()+" "+ monthNames[today.getMonth()]+"<br />"+today.getFullYear();
+	// Simple date
+	document.getElementById('date').innerHTML = h+":"+m+":"+s+"<br />"+today.getDate()+"/"+ (today.getMonth()+1)+"/"+today.getFullYear();
 	t = setTimeout('startTime()',500);
 }
 
 // RANDOM AVATAR //
-var avImages = [ 'png/png_av1.png', 'png/png_av2.png', 'png/png_av3.png', //png
+var avImages = [ 'png/png_av1.png', 'png/png_av2.png', //png
                  'jpg/jpg_av1.jpg', 'jpg/jpg_av2.jpg', 'jpg/jpg_av3.jpg', 'jpg/jpg_av4.jpg', //jpg
                  'jpg/jpg_av5.jpg', 'jpg/jpg_av6.jpg', 'jpg/jpg_av7.jpg', 'jpg/jpg_av8.jpg', //jpg
-                 'gif/gif_av1.gif', 'gif/gif_av2.gif', 'gif/gif_av3.gif', 'gif/gif_av4.gif', //gif
-                 'gif/gif_av5.gif' ]; //gif
+                 'gif/gif_av1.gif', 'gif/gif_av2.gif', 'gif/gif_av3.gif' ]; //gif
 var p = avImages.length;
 var avPreBuffer = new Array();
-for (i = 0; i < p; i++) {
+for (i = 0; i < p; i++){
 	avPreBuffer[i] = new Image();
 	avPreBuffer[i].src = avImages[i];
 }
+var avImage = Math.round(Math.random()*(p-1));
 function showAvatar() {
-	var avImage = Math.round(Math.random() * (p - 1));
-	document.getElementById('avatar_img').src = 'img/avatars/' + avImages[avImage];
-	t = setTimeout('showAvatar()',900000); // Update after 15m
+	document.getElementById('avatar_img').src = 'img/avatars/'+avImages[avImage];
 }
 
 //////////////
 // IMPORTED //
 //////////////
-
-// Cross-browser event handling, by Scott Andrew //
+/* 
+ * Cross-browser event handling, by Scott Andrew
+ */
 function addEvent(element, eventType, lamdaFunction, useCapture) {
     if (element.addEventListener) {
         element.addEventListener(eventType, lamdaFunction, useCapture);
