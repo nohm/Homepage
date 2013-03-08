b3ba111d452d38520accd9586fc70b7386c345e0
Steven Koolhaas <snack93@lavabit.com>
2013-03-08 20:36:27 +0100

    More changes

--- js/js.js
+++ js/js.js
@@ -1,20 +1,5 @@
-/* js.js*/
-/*                    _                                     
- ___ _ __   __ _  ___| | __  _    ___ _ __ __ _ _   _  __ _ 
-/ __| '_ \ / _` |/ __| |/ / (_)  / _ \ '__/ _` | | | |/ _` |
-\__ \ | | | (_| | (__|   <   _  |  __/ | | (_| | |_| | (_| |
-|___/_| |_|\__,_|\___|_|\_\ (_)  \___|_|  \__,_|\__, |\__,_|
-                                                |___/          
- */
-
-////////////
-// CUSTOM //
-////////////
-
-// SETUP //
 addEvent(window, 'load', startTime); 	// shows the clock
 
-// CLOCK/DATE //
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -27,11 +12,6 @@ function startTime() {
 	t = setTimeout('startTime()',500);
 }
 
-//////////////
-// IMPORTED //
-//////////////
-
-// Cross-browser event handling, by Scott Andrew //
 function addEvent(element, eventType, lamdaFunction, useCapture) {
     if (element.addEventListener) {
         element.addEventListener(eventType, lamdaFunction, useCapture);
