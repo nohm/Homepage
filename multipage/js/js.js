4f0df5ce7ec736c52ec25dfc85243f09ce02df62
Steven Koolhaas <snack93@lavabit.com>
2013-06-10 09:13:37 +0200

    Went from ids to classes for tabs, made the rotation fully modular. It now picks up new pages if you add them.

--- multipage/js/js.js
+++ multipage/js/js.js
@@ -1,64 +1,47 @@
 $(document).ready(function() {
 
 	// Random background image
+	// Add files to img/ and add the filenames to the array
 	var images = [
     	'1.jpg','2.jpg','3.jpg'
   	];
-
+  	// Picks a random background image and sets it
 	$('body').css({
 		'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'
 	});
 
 	// Page rotation
+	// It picks up all object with class 'tabbed-interface' and ids starting with 'page'
+	var pages = $('.tabbed-interface[id^=page]');
 	var shownPage = 1;
 	var fadespeed = 'slow';
 
-	$('#page2').hide();
-	$('#page3').hide();
+	// No/one pages? Hide the nav.
+	if (pages.length <= 1) {
+		$('#nav-left').hide();
+		$('#nav-right').hide();
+	}
 
+	// Hide all but the first page
+	for (var i = 1; i < pages.length; i++) {
+		$('#' + pages[i].id).hide();
+	}
+
+	// Rotate left on nav-left
 	$('#nav-left').click(function() {
-		switch (shownPage) {
-			case 1:
-				$('#page1').fadeOut(fadespeed, function() {
-					$('#page3').fadeIn();
-				});
-				shownPage = 3;
-				break;
-			case 2:
-				$('#page2').fadeOut(fadespeed, function() {
-					$('#page1').fadeIn();
-				});
-				shownPage = 1;
-				break;
-			case 3:
-				$('#page3').fadeOut(fadespeed, function() {
-					$('#page2').fadeIn();
-				});
-				shownPage = 2;
-				break;
-		}
+		rotate(shownPage, (shownPage - 1 == 0) ? pages.length : shownPage - 1);
 	});
 
+	// Rotate right on nav-right
 	$('#nav-right').click(function() {
-		switch (shownPage) {
-			case 1:
-				$('#page1').fadeOut(fadespeed, function() {
-					$('#page2').fadeIn();
-				});
-				shownPage = 2;
-				break;
-			case 2:
-				$('#page2').fadeOut(fadespeed, function() {
-					$('#page3').fadeIn();
-				});
-				shownPage = 3;
-				break;
-			case 3:
-				$('#page3').fadeOut(fadespeed, function() {
-					$('#page1').fadeIn();
-				});
-				shownPage = 1;
-				break;
-		}
+		rotate(shownPage, (shownPage + 1 > pages.length) ? 1 : shownPage + 1);
 	});
+
+	// Rotates the pages from > to
+	function rotate(from, to) {
+		$('#' + pages[from - 1].id).fadeOut(fadespeed, function() {
+			$('#' + pages[to - 1].id).fadeIn();
+		});
+		shownPage = to;
+	}
 });
