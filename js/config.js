67285872533728723ae1da2f6f25be79894e8ba8
Steven Koolhaas <snack93@lavabit.com>
2013-06-16 22:05:03 +0200

    more wps, minor css changes

--- js/config.js
+++ js/config.js
@@ -3,95 +3,35 @@
 var config = {
 	"fadespeed": "slow",
 	"images": [
-		'1.jpg',
-		'2.jpg',
-		'3.jpg',
-		'4.jpg',
-		'5.jpg',
-		'6.jpg',
-		'7.jpg',
-		'8.jpg',
-		'9.jpg',
-		'10.jpg',
-		'11.jpg',
-		'12.jpg'
+		'1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg',
+		'7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg',
+		'13.jpg', '14.jpg', '15.jpg', '16.jpg'
 	],
 	"pages": [
 		{
 			"gridX": 3,
 			"gridY": 3,
 			"blocks": [
-				{
-					"icon": "icon-suitcase",
-					"label": "School",
-					"link": "http://leren.saxion.nl/"
-				},
-				{
-					"icon": "icon-envelope",
-					"label": "Mail",
-					"link": "https://mail.google.com/mail/?shva=1#inbox"
-				},
-				{
-					"icon": "icon-calendar",
-					"label": "Calendar",
-					"link": "https://www.google.com/calendar/render?tab=mc&amp;pli=1"
-				},
-				{
-					"icon": "icon-rss",
-					"label": "TT-RSS",
-					"link": "http://localhost/tt-rss"
-				},
-				{
-					"icon": "icon-github",
-					"label": "GitHub",
-					"link": "https://github.com/"
-				},
-				{
-					"icon": "icon-map-marker",
-					"label": "GPS-Buddy",
-					"link": "http://gps-buddy.com/"
-				},
-				{
-					"icon": "icon-list-alt",
-					"label": "Nu.nl",
-					"link": "http://nu.nl/"
-				},
-				{
-					"icon": "icon-bar-chart",
-					"label": "Hacker News",
-					"link": "https://news.ycombinator.com/"
-				},
-				{
-					"icon": "icon-laptop",
-					"label": "GW2Guru",
-					"link": "http://guildwars2guru.com/"
-				}
+				{ "icon": "icon-suitcase",		"label": "School",			"link": "http://leren.saxion.nl/" 									},
+				{ "icon": "icon-envelope",		"label": "Mail",			"link": "https://mail.google.com/mail/?shva=1#inbox" 				},
+				{ "icon": "icon-calendar",		"label": "Calendar",		"link": "https://www.google.com/calendar/render?tab=mc&amp;pli=1" 	},
+				{ "icon": "icon-rss",			"label": "TT-RSS",			"link": "http://localhost/tt-rss" 									},
+				{ "icon": "icon-github",		"label": "GitHub",			"link": "https://github.com/" 										},
+				{ "icon": "icon-map-marker",	"label": "GPS-Buddy",		"link": "http://gps-buddy.com/" 									},
+				{ "icon": "icon-list-alt",		"label": "Nu.nl",			"link": "http://nu.nl/" 											},
+				{ "icon": "icon-bar-chart",		"label": "Hacker News",		"link": "https://news.ycombinator.com/" 							},
+				{ "icon": "icon-laptop",		"label": "GW2Guru",			"link": "http://guildwars2guru.com/" 								}
 			]
 		},
 		{
-			"gridX": 2,
+			"gridX": 3,
 			"gridY": 2,
 			"blocks": [
-				{
-					"icon": "icon-laptop",
-					"label": "Technology",
-					"link": "http://4chan.org/g/"
-				},
-				{
-					"icon": "icon-picture",
-					"label": "Wallpapers / General",
-					"link": "http://4chan.org/wg/"
-				},
-				{
-					"icon": "icon-play",
-					"label": "Last.fm",
-					"link": "http://last.fm"
-				},
-				{
-					"icon": "icon-film",
-					"label": "Tinychat",
-					"link": "http://tinychat.com/haremchat"
-				}
+				{ "icon": "icon-laptop", 	"label": "Technology", 				"link": "http://4chan.org/g/"					},
+				{"icon": "icon-picture",	"label": "Wallpapers / General", 	"link": "http://4chan.org/wg/" 					},
+				{ "icon": "icon-play", 		"label": "Last.fm", 				"link": "http://last.fm" 						},
+				{ "icon": "icon-film", 		"label": "Tinychat", 				"link": "http://tinychat.com/haremchat" 		},
+				{ "icon": "icon-cloud", 	"label": "forecast.io", 			"link": "http://forecast.io/#/f/52.2200,6.8894" }
 			]
 		}
 	]
